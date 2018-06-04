1fdc08136ddd737c4c46f977391bb95c843f0a19
ShanuMaurya <36756132+ShanuMaurya@users.noreply.github.com>
2018-06-04 12:15:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NDM3MjE5ODgwMTc4MDI0NDQ4.DfMngw.aiilp7si6bfKWYaEgBqm-wcPn5k';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = 'ot!';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyC-vW6v72JdLtPbZkbac-DZtt6jf183Ja4';
